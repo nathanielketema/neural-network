c8cb6415cc47091198d304bcf5a203e469192d4c
Nathaniel Ketema <nathanielketema@gmail.com>
2026-02-05 09:21:36 -0600

    refactor: remove uneccessary deinit() calls

--- src/matrix.zig
+++ src/matrix.zig
@@ -231,7 +231,6 @@ test "multiply" {
     var b: Matrix(f32) = .create(&b_data, .to_enum(2), .to_enum(3));
 
     var result: Matrix(f32) = try .init(fba, .to_enum(2), .to_enum(3));
-    defer result.deinit(fba);
 
     matrix_multiply(f32, &result, &a, &b);
 
@@ -248,11 +247,9 @@ test "clone" {
     const col: Matrix(f32).Col = .to_enum(3);
 
     var a: Matrix(f32) = try .init(fba, row, col);
-    defer a.deinit(fba);
     a.fill(23);
 
     var b: Matrix(f32) = try .init(fba, row, col);
-    defer b.deinit(fba);
 
     matrix_copy(f32, &b, a);
 
