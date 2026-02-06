8f44f5b32048750023d5d728976a3c3420a622ce
Nathaniel Ketema <nathanielketema@gmail.com>
2026-02-06 12:45:20 -0600

    feat: simplify build.zig

--- build.zig
+++ build.zig
@@ -4,16 +4,19 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const nn_mod = b.addModule("neural_network", .{
-        .root_source_file = b.path("src/neural_network.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const matrix_mod = b.addModule("matrix", .{
         .root_source_file = b.path("src/matrix.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const nn_mod = b.addModule("neural_network", .{
+        .root_source_file = b.path("src/neural_network.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "matrix", .module = matrix_mod },
+        },
     });
 
     const exe = b.addExecutable(.{
@@ -24,7 +27,6 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
             .imports = &.{
                 .{ .name = "nn", .module = nn_mod },
-                .{ .name = "matrix", .module = matrix_mod },
             },
         }),
     });
@@ -40,21 +42,19 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const nn_mod_tests = b.addTest(.{
-        .root_module = nn_mod,
-    });
-
     const matrix_mod_tests = b.addTest(.{
         .root_module = matrix_mod,
     });
-
-    const run_nn_mod_tests = b.addRunArtifact(nn_mod_tests);
     const run_matrix_mod_tests = b.addRunArtifact(matrix_mod_tests);
+
+    const nn_mod_tests = b.addTest(.{
+        .root_module = nn_mod,
+    });
+    const run_nn_mod_tests = b.addRunArtifact(nn_mod_tests);
 
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
-
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
