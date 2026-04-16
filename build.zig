69ccbb86c7c06e04ccf8e1690c4e30eb8f67c3dc
Nathaniel Ketema <nathanielketema@gmail.com>
2026-04-15 23:34:45 -0500

    chore: simplify build

--- build.zig
+++ build.zig
@@ -19,46 +19,17 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    const exe = b.addExecutable(.{
-        .name = "neural_network",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "nn", .module = nn_mod },
-            },
-        }),
-    });
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const matrix_mod_tests = b.addTest(.{
+    const matrix_test = b.addTest(.{
         .root_module = matrix_mod,
     });
-    const run_matrix_mod_tests = b.addRunArtifact(matrix_mod_tests);
+    const run_matrix_test = b.addRunArtifact(matrix_test);
 
-    const nn_mod_tests = b.addTest(.{
+    const nn_test = b.addTest(.{
         .root_module = nn_mod,
     });
-    const run_nn_mod_tests = b.addRunArtifact(nn_mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-    const run_exe_tests = b.addRunArtifact(exe_tests);
+    const run_nn_test = b.addRunArtifact(nn_test);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_nn_mod_tests.step);
-    test_step.dependOn(&run_matrix_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    test_step.dependOn(&run_nn_test.step);
+    test_step.dependOn(&run_matrix_test.step);
 }
